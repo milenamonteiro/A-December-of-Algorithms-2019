5714cba4e07a2c09bd0c3a96125e25ae6cf1fd04
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-12-31 13:22:45 -0300

    solved dec 14

--- December-14/python_milenamonteiro.py
+++ December-14/python_milenamonteiro.py
@@ -1,6 +1,14 @@
 def wordplay(s):
     score_a = 0
     score_b = 0
-    return s
-
-print(wordplay("london"))
+    for x in range(len(s)):
+        for i in range(len(s)+1):
+            if s[x:i]:
+                if s[x:i][0] in "aeiou":
+                    score_a += 1
+                else:
+                    score_b += 1
+    if score_b > score_a:
+        print("The winner is B with {} points!".format(score_b))
+    else:
+        print("The winner is A with {} points!".format(score_a))
